0d8009fe68306e93c49207ab71f1bf1eb7df4b1f
Elena Chien <terrisa1993@gmail.com>
2022-04-15 16:08:59 +0800

    finish basic layout of practice start page

--- src/routes/practice/PracticeStart.js
+++ src/routes/practice/PracticeStart.js
@@ -12,12 +12,13 @@ import styled from 'styled-components';
 
 import BackButton from '../../components/elements/BackButton';
 import NoteBar from '../../components/elements/NoteBar';
+import SwitchElement from '../../components/elements/Switch';
 
 const Container = styled.div`
-  display: flex;  
+  display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 const TitleWrapper = styled.div`
   width: 100%;
@@ -28,7 +29,7 @@ const TitleWrapper = styled.div`
 
 const StyledNoteBar = styled(NoteBar)`
   width: 40%;
-`
+`;
 
 const Question = styled.div`
   font-size: 30px;
@@ -37,26 +38,52 @@ const Question = styled.div`
   margin-bottom: 30px;
 `;
 
-const TimerSettingWrapper = styled.div``;
+const PlayerWrapper = styled.div`
+  margin-bottom: 20px;
+`;
+
+const TimerSettingWrapper = styled.div`
+  display: flex;
+  width: 40%;
+  margin-bottom: 20px;
+  justify-content: center;s
+`;
 
 const PracticeStart = () => {
   const [currentQuestion, setCurrentQuestion] = useState('');
+  const [isTimer, setIsTimer] = useState(true);
+  const [timer, setTimer] = useState(3);
   const props = useOutletContext();
 
   useEffect(() => {
     setCurrentQuestion(props.practiceQuestions[0].question);
   }, []);
 
-  console.log(currentQuestion);
+  console.log(props.practiceQuestions, currentQuestion);
   return (
     <Container>
       <TitleWrapper>
-        <BackButton path="/practice" isStart/>
+        <BackButton path="/practice" isStart />
         <StyledNoteBar brief={props.brief} />
       </TitleWrapper>
       <Question>{currentQuestion}</Question>
-      <h1>Player</h1>
-      {/* <TimerSettingWrapper></TimerSettingWrapper> */}
+      <PlayerWrapper>
+        {props.recordType === '錄音' ? '錄音 Player' : '錄影 Player'}
+      </PlayerWrapper>
+      <TimerSettingWrapper>
+        <SwitchElement isTimer={isTimer} setIsTimer={setIsTimer} />
+        {isTimer ? (
+          <>
+            <p>此題定時</p>
+            <div>
+              <input value={timer} onChange={(e) => setTimer(e.target.value)} />
+              <p>分鐘</p>
+            </div>
+          </>
+        ) : (
+          <p>此題不定時（回答時間上限為 3 分鐘）</p>
+        )}
+      </TimerSettingWrapper>
       <Button
         size="sm"
         // onClick={() => }
